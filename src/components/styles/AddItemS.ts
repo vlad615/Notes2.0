d2b4777d2fb107a432b075faaa1d1a2a01fb1492
VladislavAmbrosov <ambrosov413@gmail.com>
2026-07-10 08:20:37 +0300

    adding initial styles

---
+++ src/components/styles/AddItemS.ts
@@ -0,0 +1,8 @@
+import { styled } from '@mui/material/styles'
+import TextField from '@mui/material/TextField';
+
+export const AddItemS = styled(TextField)({
+    width: '100%',
+    marginRight: '10px'
+
+})
---
+++ src/components/styles/Buttons.ts
@@ -0,0 +1,6 @@
+import { styled } from '@mui/material/styles'
+import Button from '@mui/material/Button';
+
+export const ButtonS = styled(Button)({
+    
+})
