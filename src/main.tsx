fe8032cbda9b3ebb6a46c5cd6cc0b64668e8c833
VladislavAmbrosov <ambrosov413@gmail.com>
2026-07-15 12:26:05 +0300

    refactor main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import { App } from './app/App.tsx'
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <Provider store={store}>
     <StrictMode>
       <App />
     </StrictMode>
-  </Provider>
-  ,
-)
+  </Provider>);
