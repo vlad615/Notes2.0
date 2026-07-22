885207bc3f58bfa6dabc1454d09966ad0b5ae9fd
VladislavAmbrosov <ambrosov413@gmail.com>
2026-07-22 13:08:03 +0300

    api for todolists

--- src/commun/instance/instance.ts
+++ src/commun/instance/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const token = '234c58b8-0848-4abe-bf26-f0ff64ceb548'
-const api = 'e5666eb3-83be-46ad-b38a-45c04b0fa9c4'
+const token = ''
+const api = ''
 
 export const instance = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.1',
