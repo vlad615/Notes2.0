1f2c7f1edf0a32b1e4729d44c0fc38206f9f213b
VladislavAmbrosov <ambrosov413@gmail.com>
2026-07-25 11:06:56 +0300

    add Thunk to todolists slice
    -- create func fetchTodolistsTC and extraReducers for adding data to store.
    	this func gets list of todolists from backend.
    -- changed ListType concat backend's types with filter status
    
    Useg fetchTodolistsTC in CardsList with useEffect

--- src/features/todolists/model/todolists-slice.ts
+++ src/features/todolists/model/todolists-slice.ts
@@ -1,13 +1,15 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, nanoid } from '@reduxjs/toolkit'
+import { todolistsApi, type Todolist } from '../api'
 
 export const todolistsSlice = createSlice({
     name: 'todolists',
     initialState: {
-        todoLists: [] as Todolist[],
+        todoLists: [] as ListType[],
     },
     selectors: {
         selectLists: (state) => state.todoLists,
     },
+
     reducers: (create) => ({
         deleteTodolistAC: create.reducer<{ id: string }>((state, action) => {
             const index = state.todoLists.findIndex((todolist) => todolist.id === action.payload.id)
@@ -29,7 +31,7 @@ export const todolistsSlice = createSlice({
         }),
         createTodolistAC: create.preparedReducer(
             (title: string) => {
-                const newTodolist: Todolist = { id: nanoid(), title, filter: 'all' }
+                const newTodolist: ListType = { id: nanoid(), title, filter: 'all', addedDate: Date(), order: 12 }
                 return { payload: newTodolist }
             },
             (state, action) => {
@@ -37,6 +39,21 @@ export const todolistsSlice = createSlice({
             },
         ),
     }),
+    extraReducers: (builder) => {
+        builder.addCase(fetchTodolistsTC.fulfilled, (_state, action) => ({
+            todoLists: action.payload.map((td) => ({ ...td, filter: 'all' })),
+        }))
+    },
+})
+
+export const fetchTodolistsTC = createAsyncThunk(`${todolistsSlice.name}/fetchTodolistsTC`, async () => {
+    const res = await todolistsApi.getTodoLists()
+    return res.data
+})
+
+export const addTodoListsTC = createAsyncThunk(`${todolistsSlice.name}/fetchTodolistsTC`, async () => {
+    const res = await todolistsApi.getTodoLists()
+    return res.data
 })
 
 export const { deleteTodolistAC, createTodolistAC, changeTodolistFilterAC, changeTodolistTitleAC } =
@@ -44,14 +61,7 @@ export const { deleteTodolistAC, createTodolistAC, changeTodolistFilterAC, chang
 export const { selectLists } = todolistsSlice.selectors
 export const todolistsReducer = todolistsSlice.reducer
 
-export type Todolist = {
-    id: string
-    title: string
-    filter: Filter
-}
-export type ListType = {
-    id: string
-    title: string
+export type ListType = Todolist & {
     filter: Filter
 }
 
--- src/features/todolists/ui/ToDoLists/CardsList/CardsList.tsx
+++ src/features/todolists/ui/ToDoLists/CardsList/CardsList.tsx
@@ -2,15 +2,20 @@ import { useAppDispatch, useAppSelector } from '@/commun/hooks'
 import { Box, Paper } from '@mui/material'
 import s from './CardsList.module.css'
 import { AddItem } from '@/commun/components/'
-import { createTaskAC, type TaskProps, type ListType, selectLists } from '@/features/todolists/model'
+import { createTaskAC, type TaskProps, type ListType, selectLists, fetchTodolistsTC } from '@/features/todolists/model'
 import { FilterButtons } from './FilterButtons/FilterButtons'
 import { Tasks } from './Tasks/Tasks'
 import { CardHeader } from './CardHeader/CardHeader'
+import { useEffect } from 'react'
 
 export const CardsList = () => {
     const lists = useAppSelector(selectLists)
-
     const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        const asyncAction = fetchTodolistsTC()
+        dispatch(asyncAction)
+    }, [])
 
     function createTask(id: ListType['id'], title: TaskProps['title']) {
         dispatch(createTaskAC({ todolistId: id, title }))
